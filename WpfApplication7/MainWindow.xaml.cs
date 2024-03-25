da4d35945273342b1554a830731f4dc2a7aca9f3
Кирилл <@DESKTOP-MPM09EH>
2024-03-25 22:51:56 +0500

    Добавьте файлы проекта.

---
+++ WpfApplication7/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication7
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            bSend.IsEnabled = false;
+        }
+
+        private void dpBirghtday_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(tbAge.Text != "" & tbName.Text != "" & tbNumber.Text != "" & tbSubName.Text != "")
+            {
+                bSend.IsEnabled = true;
+            }
+            else
+            {
+                bSend.IsEnabled = false;
+            }
+        }
+
+        private void bSend_Click(object sender, RoutedEventArgs e)
+        {
+            User usr = new User(tbName.Text,tbSubName.Text,int.Parse(tbAge.Text),int.Parse(tbNumber.Text),dpBirghtday.Text);
+            MessageBox.Show(usr.Show());
+        }
+    }
+}
---
+++ WpfApplication7/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication7
+{
+    class User
+    {
+        static public List<User> Users = new List<User>();
+        string Name { get; set; }
+        string SubName { get; set; }
+        int Age { get; set; }
+        int Number { get; set; }
+        string Birghtday { get; set; }
+        public User(string _name,string _subName, int _age,int _number,string _birghtday)
+        {
+            Name = _name;
+            SubName = _subName;
+            Age = _age;
+            Number = _number;
+            Birghtday = _birghtday;
+        }
+        public string Show()
+        {
+            return $"Имя: {Name}\nФамилия: {SubName}\nВозраст: {Age}\nНомер телефона: {Number}\nДень рождения: {Birghtday}";
+        }
+    }
+}
